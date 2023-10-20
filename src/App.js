f0a2c5d50c7417f66934ec3ed234769011f2bfba
unknown <saimaniaaki402@gmail.com>
2023-10-20 14:35:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Axios from './Components/Axios';
+// import Calculator2 from './Components/Calculator2';
+// import Fetch from './Components/Fetch';
+// import Filter from './Components/Filter';
+// import LoginForm from './Components/LoginForm';
+// import SigninForm from './Components/SigninForm';
+ 
+ 
+function App() {
+  return (
+    <div className="App">
+      {/* <Welcome/> */}
+      {/* <Useeffect/> */}
+      {/* <Nameclick/> */}
+      {/* <Onsubmit/> */}
+      {/* <Map/> */}
+      {/* <Filter/> */}
+      {/* <LoginForm/> */}
+      {/* <SigninForm/> */}
+      {/* <Calculator2/> */}
+    {/* <Fetch/> */}
+    <Axios/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Axios.js
@@ -0,0 +1,23 @@
+import React,{useEffect,useState} from 'react'
+import '../App.css'
+import axios from 'axios';
+const Axios = () =>{
+    const[data,setData]=useState([]);
+
+    useEffect(()=>{
+        axios.get("https://dummyjson.com/products")
+        .then (res => {
+            console.log(res?.data.products,"json")
+            setData(res?.data.products)})
+    },[])
+  return (
+    <div>
+        <ul>
+        <h4>{data.map(item=><p key={item.id}> <img src={item.images[0]} alt={item.name} className='image'/><br/>{item.title}    <br /> {item.price}<br/> {item.brand} <br/><br/>  <button onClick={()=>{alert("first check your wallet ")}}>Buy Now</button> </p>
+        )}</h4>
+        </ul>
+    </div>
+  )
+}
+
+export default Axios 
