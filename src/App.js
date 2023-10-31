637a9c84f9fc0d4c8f0382dc7e17beb6826c4e1b
unknown <saimaniaaki402@gmail.com>
2023-10-31 12:38:20 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
+import React from 'react';
 import './App.css';
-import Database from './Components/Database';
-// import Axios from './Components/Axios';
-// import Stopwatch from './Components/Stopwatch';
-// import Calculator2 from './Components/Calculator2';
-// import Fetch from './Components/Fetch';
-// import Filter from './Components/Filter';
-// import LoginForm from './Components/LoginForm';
-// import SigninForm from './Components/SigninForm';
+import { BrowserRouter } from 'react-router-dom';
  
- 
-function App() {
+// import Nameclick from './Components/Nameclick';
+import Axios from './Components/Axios';
+
+  
+const App = () => {
   return (
-    <div className="App">
-      {/* <Welcome/> */}
-      {/* <Useeffect/> */}
-      {/* <Nameclick/> */}
-      {/* <Onsubmit/> */}
-      {/* <Map/> */}
-      {/* <Filter/> */}
-      {/* <LoginForm/> */}
-      {/* <SigninForm/> */}
-      {/* <Calculator2/> */}
-    {/* <Fetch/> */}
-    {/* <Axios/> */}
-    {/* <Stopwatch/> */}
-    <Database/>
+    <div className='App'> 
+    {/* <Header/>
+    <Body/> */}
+    {/* <Download/> */}
+    {/* <SearchFilter/> */}
+    <BrowserRouter>
+    <Axios/>
+    </BrowserRouter>
+    {/* <Nameclick/> */}
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/Axios.js
+++ src/Components/Axios.js
@@ -1,22 +1,52 @@
-import React,{useEffect,useState} from 'react'
-import '../App.css'
+import React, { useEffect, useState } from 'react';
+import '../App.css';
 import axios from 'axios';
-const Axios = () =>{
-    const[data,setData]=useState([]);
-    useEffect(()=>{
-        axios.get("https://dummyjson.com/products")
-        .then (res => {
-            console.log(res?.data.products,"json")
-            setData(res?.data.products)})
-    },[])
+import Header from '../Firstapp/Header';
+const Axios = () => {
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);  
+
+  useEffect(() => {
+    axios.get("https://dummyjson.com/products")
+      .then(res => {
+        console.log(res?.data.products, "json");
+        setData(res?.data.products);
+        setFilteredData(res?.data.products);  
+      })
+  }, []);
+
+  const HighCostItems = () => {
+    const filteredItems = data.filter(item => item.price > 550);
+    setFilteredData(filteredItems);
+  };
+  const LowCostItems = () => {
+    const filteredItems1 = data.filter(item => item.price < 550);
+    setFilteredData(filteredItems1);
+  };
+
   return (
     <div>
+      <Header/>
+      <div className='filter-btn'>
+        <button onClick={HighCostItems}> High cost Items</button>
+        <button onClick={LowCostItems}>Low cost Items</button>
+      </div>
+      <div>
         <ul>
-        <h4>{data.map(item=><p className='mainDiv' key={item.id}> <img src={item.images[0]} alt={item.name} className='image'/><br/>{item.title}    <br /> {item.price}<br/> {item.brand} <br/><br/>  <button onClick={()=>{alert("first check your wallet 😀 ")}}>Buy Now</button> </p>
-        )}</h4>
+          <h4>
+            {filteredData.map(item => (
+              <p className='mainDiv' key={item.id}>
+                <img src={item.images[0]} alt={item.name} className='image' /><br />
+                {item.title} <br />
+                {item.price}<br /> {item.brand} <br /><br />
+                <button onClick={() => { alert("first check your wallet 😀") }}>Buy Now</button>
+              </p>
+            ))}
+          </h4>
         </ul>
+      </div>
     </div>
   )
 }
 
-export default Axios 
+export default Axios;
